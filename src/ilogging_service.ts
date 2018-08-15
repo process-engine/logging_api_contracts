7c450668066af4f10d59f9123bc25e7a0fd29ab8
ElRaptorus <christian.werner@5minds.de>
2018-08-15 14:31:17 +0200

    Add identity to interface signatures

--- src/ilogging_service.ts
+++ src/ilogging_service.ts
@@ -1,8 +1,14 @@
-import {LogLevel} from "./log_level";
-import {LogEntry} from "./log_entry";
+import {LogEntry} from './log_entry';
+import {LogLevel} from './log_level';
+
+import {IIdentity} from '@essential-projects/iam_contracts';
 
 export interface ILoggingService {
+  getLogsForProcessInstance(identity: IIdentity, processModelId: string, correlationId: string, logLevel?: LogLevel): Array<LogEntry>;
   writeLogForProcessModel(processModelId: string, correlationId: string, logLevel: LogLevel, message: string): Promise<void>;
-  writeLogForFlowNodeInstance(processModelId: string, correlationId: string, flowNodeInstanceId: string, logLevel: LogLevel, message: string): Promise<void>;
-  getLogsForProcessInstance(processModelId: string, correlationId: string, logLevel?: LogLevel): Array<LogEntry>;
+  writeLogForFlowNodeInstance(processModelId: string,
+                              correlationId: string,
+                              flowNodeInstanceId: string,
+                              logLevel: LogLevel,
+                              message: string): Promise<void>;
 }
--- src/log_entry.ts
+++ src/log_entry.ts
@@ -1,10 +1,10 @@
-import {LogLevel} from "./log_level";
+import {LogLevel} from './log_level';
 
 export class LogEntry {
-  processModelId: string;
-  correlationId: string;
-  flowNodeInstanceId?: string;
-  logLevel: LogLevel;
-  message: string;
-  timeStamp: Date;
+  public processModelId: string;
+  public correlationId: string;
+  public flowNodeInstanceId?: string;
+  public logLevel: LogLevel;
+  public message: string;
+  public timeStamp: Date;
 }
