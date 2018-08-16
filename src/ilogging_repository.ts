3f9af662359b63d1dcc87384fa4ffb623c09d1d5
ElRaptorus <christian.werner@5minds.de>
2018-08-16 11:08:54 +0200

    Add interface for logging repository

---
+++ src/ilogging_repository.ts
@@ -0,0 +1,8 @@
+import {ILoggingService} from './ilogging_service';
+
+/**
+ * Contains functions for writing and retrieving content from logfiles.
+ * Each logfile relates to a specific process model and correlation.
+ */
+export interface ILoggingRepository extends ILoggingService {
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './ilogging_repository';
 export * from './ilogging_service';
 export * from './log_entry';
 export * from './log_level';
