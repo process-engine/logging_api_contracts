0f744f03f725a6e1c47c07a2407ccc64e051430a
ElRaptorus <christian.werner@5minds.de>
2018-09-26 09:46:38 +0200

    :sparkles: Add processInstanceId to signatures

--- src/ilogging_api.ts
+++ src/ilogging_api.ts
@@ -23,17 +23,19 @@ export interface ILoggingApi {
   /**
    * Writes a log entry for a specific ProcessModel of a Correlation.
    * @async
-   * @param correlationId  The ID of the Correlation to which the
-   *                       ProcessModel belongs.
-   * @param processModelId The ID of ProcessModel for which to create a
-   *                       log entry.
-   * @param logLevel       The loglevel to use.
-   * @param message        The message to write into the log entry.
-   * @param timestamp      Optional: The timestamp to use for the log entry.
-   *                       Defaults to "now".
+   * @param correlationId     The ID of the Correlation to which the
+   *                          ProcessModel belongs.
+   * @param processModelId    The ID of ProcessModel for which to create a
+   *                          log entry.
+   * @param processInstanceId The instance ID of the ProcessModel.
+   * @param logLevel          The loglevel to use.
+   * @param message           The message to write into the log entry.
+   * @param timestamp         Optional: The timestamp to use for the log entry.
+   *                          Defaults to "now".
    */
   writeLogForProcessModel(correlationId: string,
                           processModelId: string,
+                          processInstanceId: string,
                           logLevel: LogLevel,
                           message: string,
                           timestamp?: Date): Promise<void>;
@@ -46,6 +48,7 @@ export interface ILoggingApi {
    *                           ProcessModel belongs.
    * @param processModelId     The ID of ProcessModel to which the FlowNode
    *                           belongs.
+   * @param processInstanceId  The instance ID of the ProcessModel.
    * @param flowNodeInstanceId The instance ID of FlowNode for which to create
    *                           a log entry.
    * @param flowNodeId         The ID of FlowNode for which to create a
@@ -57,6 +60,7 @@ export interface ILoggingApi {
    */
   writeLogForFlowNode(correlationId: string,
                       processModelId: string,
+                      processInstanceId: string,
                       flowNodeInstanceId: string,
                       flowNodeId: string,
                       logLevel: LogLevel,
